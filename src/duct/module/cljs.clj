e4273a1ce29fee432867db782650a9e0bcdfce08
James Reeves <jreeves@weavejester.com>
2017-02-22 23:15:09 +0000

    Add :duct.module/cljs key

--- src/duct/module/cljs.clj
+++ src/duct/module/cljs.clj
@@ -1,6 +1,21 @@
-(ns duct.module.cljs)
+(ns duct.module.cljs
+  (:require [integrant.core :as ig]
+            [duct.compiler.cljs :as cljs]
+            [duct.core :refer [assoc-in-default]]
+            [duct.server.figwheel :as figwheel]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def ^:private build
+  {:source-paths  ["src"]
+   :build-options {:output-to  "target/duct/js/main.js"
+                   :output-dir "target/duct/js"
+                   :optimizations :whitespace}})
+
+(defmethod ig/init-key ::build [_ options] options)
+
+(defmethod ig/init-key :duct.module/cljs [_ _]
+  (fn [config]
+    (-> config
+        (assoc ::build build)
+        (assoc-in-default [:duct.server/figwheel :css-dirs] ["dev/resources"])
+        (assoc-in-default [:duct.server/figwheel :builds]   [(ig/ref ::build)])
+        (assoc-in-default [:duct.compiler/cljs :builds]     [(ig/ref ::build)]))))
