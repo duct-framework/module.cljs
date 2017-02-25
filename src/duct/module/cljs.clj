34db347de93806cb9e1b436292dd622b10fcfba3
James Reeves <jreeves@weavejester.com>
2017-02-25 20:06:24 +0000

    Add :main key to module options

--- src/duct/module/cljs.clj
+++ src/duct/module/cljs.clj
@@ -4,33 +4,35 @@
             [duct.core :refer [assoc-in-default]]
             [duct.server.figwheel :as figwheel]))
 
-(def ^:private compiler-build
+(defn- compiler-build [{:keys [main]}]
   {:source-paths  ["src"]
-   :build-options {:output-to  "target/duct/js/main.js"
-                   :output-dir "target/duct/js"
+   :build-options {:main       main
+                   :output-to  "target/assets/js/main.js"
+                   :output-dir "target/assets/js"
                    :optimizations :advanced}})
 
-(def ^:private figwheel-build
+(defn- figwheel-build [{:keys [main]}]
   {:id            ::build
    :figwheel      true
    :source-paths  ["src"]
-   :build-options {:output-to  "target/duct/js/main.js"
-                   :output-dir "target/duct/js"
+   :build-options {:main       main
+                   :output-to  "target/assets/js/main.js"
+                   :output-dir "target/assets/js"
                    :asset-path "js"
                    :optimizations :none}})
 
-(defn- assoc-compiler [config]
+(defn- assoc-compiler [config options]
   (-> config
-      (assoc-in-default [:duct.compiler/cljs :builds] [compiler-build])))
+      (assoc-in-default [:duct.compiler/cljs :builds] [(compiler-build options)])))
 
-(defn- assoc-figwheel [config]
+(defn- assoc-figwheel [config options]
   (-> config
       (assoc-in-default [:duct.server/figwheel :css-dirs] ["dev/resources"])
-      (assoc-in-default [:duct.server/figwheel :builds]   [figwheel-build])))
+      (assoc-in-default [:duct.server/figwheel :builds]   [(figwheel-build options)])))
 
 (defmethod ig/init-key :duct.module/cljs [_ options]
   (fn [config]
     (let [env (:environment options (:duct.core/environment config :production))]
       (cond-> config
-        (= env :production)  (assoc-compiler)
-        (= env :development) (assoc-figwheel)))))
+        (= env :production)  (assoc-compiler options)
+        (= env :development) (assoc-figwheel options)))))
