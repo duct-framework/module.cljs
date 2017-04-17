8ea36254609ebcca8b0cd91c35ac76b00493858f
James Reeves <jreeves@weavejester.com>
2017-04-17 18:12:09 +0100

    Update Duct dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,6 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.494"]
                  [binaryage/devtools "0.9.2"]
-                 [duct/core "0.9.0-SNAPSHOT"]
-                 [duct/compiler.cljs "0.1.0-SNAPSHOT"]
-                 [duct/server.figwheel "0.1.0-SNAPSHOT"]
-                 [meta-merge "1.0.0"]])
+                 [duct/core "0.1.0"]
+                 [duct/compiler.cljs "0.1.0"]
+                 [duct/server.figwheel "0.1.1"]])
