8eb9863162aa64be3f78010a97a61243a23f3dbd
James Reeves <jreeves@weavejester.com>
2017-04-17 19:04:16 +0100

    Add basic module tests

--- test/duct/module/cljs_test.clj
+++ test/duct/module/cljs_test.clj
@@ -1,7 +1,48 @@
 (ns duct.module.cljs-test
-  (:require [clojure.test :refer :all]
-            [duct.module.cljs :refer :all]))
+  (:require [clojure.java.io :as io]
+            [clojure.test :refer :all]
+            [duct.core :as core]
+            [duct.module.cljs :as cljs]
+            [integrant.core :as ig]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def base-config
+  {:duct.core/project-ns 'foo
+   :duct.core/modules    [(ig/ref :duct.module/cljs)]
+   :duct.module/cljs     {:main 'foo.client}})
+
+(defn- absolute-path [relative-path]
+  (.getAbsolutePath (io/file relative-path)))
+
+(deftest module-test
+  (testing "production config"
+    (is (= (core/prep base-config)
+           (merge base-config
+                  {:duct.compiler/cljs
+                   {:builds
+                    [{:source-paths ["src"]
+                      :build-options
+                      {:main       'foo.client
+                       :output-to  (absolute-path "target/resources/foo/public/js/main.js")
+                       :output-dir (absolute-path "target/resources/foo/public/js")
+                       :asset-path "js"
+                       :verbose    true
+                       :optimizations :advanced}}]}}))))
+
+  (testing "development config"
+    (let [config (assoc base-config ::core/environment :development)]
+      (is (= (core/prep config)
+           (merge config
+                  {:duct.server/figwheel
+                   {:css-dirs ["resources" "dev/resources"]
+                    :builds
+                    [{:id           "dev"
+                      :figwheel     true
+                      :source-paths ["dev/src" "src"]
+                      :build-options
+                      {:main       'foo.client
+                       :output-to  (absolute-path "target/resources/foo/public/js/main.js")
+                       :output-dir (absolute-path "target/resources/foo/public/js")
+                       :asset-path "js"
+                       :verbose    true
+                       :preloads   ['devtools.preload]
+                       :optimizations :none}}]}}))))))
