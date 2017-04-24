5aed291b837db265082c96f19ced2318ef3bad01
James Reeves <jreeves@weavejester.com>
2017-04-24 17:22:48 +0100

    Add Integrant 0.4.0 dependency

--- project.clj
+++ project.clj
@@ -8,4 +8,5 @@
                  [binaryage/devtools "0.9.2"]
                  [duct/core "0.1.0"]
                  [duct/compiler.cljs "0.1.0"]
-                 [duct/server.figwheel "0.1.1"]])
+                 [duct/server.figwheel "0.1.1"]
+                 [integrant "0.4.0"]])
