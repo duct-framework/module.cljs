48cfa274bd7497197b445cad40e7bb3fa51162ff
James Reeves <jreeves@weavejester.com>
2017-05-31 17:44:37 +0100

    Update Clojure and ClojureScript versions

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/duct-framework/module.cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.521"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.562"]
                  [binaryage/devtools "0.9.2"]
                  [duct/core "0.2.1"]
                  [duct/compiler.cljs "0.1.0"]
