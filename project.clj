ebca43ab5613f23852179ef86e7f96e68557ca5e
James Reeves <jreeves@weavejester.com>
2017-05-31 22:35:32 +0100

    Update compiler.cljs to 0.1.1

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
                  [org.clojure/clojurescript "1.9.562"]
                  [binaryage/devtools "0.9.2"]
                  [duct/core "0.3.3"]
-                 [duct/compiler.cljs "0.1.0"]
+                 [duct/compiler.cljs "0.1.1"]
                  [duct/server.figwheel "0.1.1"]
                  [integrant "0.4.0"]])
