82cbb7182af52435c166e6f81564f61781278106
James Reeves <jreeves@weavejester.com>
2017-06-02 19:51:57 +0100

    Update compiler and Figwheel dependencies

--- project.clj
+++ project.clj
@@ -7,6 +7,6 @@
                  [org.clojure/clojurescript "1.9.562"]
                  [binaryage/devtools "0.9.2"]
                  [duct/core "0.3.3"]
-                 [duct/compiler.cljs "0.1.1"]
-                 [duct/server.figwheel "0.1.2"]
+                 [duct/compiler.cljs "0.1.2"]
+                 [duct/server.figwheel "0.1.3"]
                  [integrant "0.4.0"]])
