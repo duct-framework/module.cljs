f4dc0ad3b0493c2e474da2dae1d086f7929c4362
James Reeves <jreeves@weavejester.com>
2017-06-02 19:52:16 +0100

    Update duct/core to 0.4.0

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.562"]
                  [binaryage/devtools "0.9.2"]
-                 [duct/core "0.3.3"]
+                 [duct/core "0.4.0"]
                  [duct/compiler.cljs "0.1.2"]
                  [duct/server.figwheel "0.1.3"]
                  [integrant "0.4.0"]])
