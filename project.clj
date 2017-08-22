1bf1466090f77cc17859f3bc45ea3f271c5bda56
James Reeves <jreeves@weavejester.com>
2017-08-22 15:40:58 +0100

    Update binaryage/devtools to 0.9.3

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
                  [org.clojure/clojurescript "1.9.908"]
-                 [binaryage/devtools "0.9.2"]
+                 [binaryage/devtools "0.9.3"]
                  [duct/core "0.6.0"]
                  [duct/compiler.cljs "0.2.0"]
                  [duct/server.figwheel "0.2.0"]
