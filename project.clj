89b94deed4bc7eea51f367a08baa61c1e1762258
James Reeves <jreeves@weavejester.com>
2017-09-03 22:09:45 +0100

    Release 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject duct/module.cljs "0.3.0"
+(defproject duct/module.cljs "0.3.1"
   :description "Duct module for developing and compiling ClojureScript"
   :url "https://github.com/duct-framework/module.cljs"
   :license {:name "Eclipse Public License"
