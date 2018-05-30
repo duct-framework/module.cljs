505f4138168b63383837473206af9ff66cab68cd
James Reeves <jreeves@weavejester.com>
2018-05-30 01:04:27 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.9.946"]
                  [binaryage/devtools "0.9.10"]
-                 [duct/core "0.7.0-alpha2"]
+                 [duct/core "0.7.0-alpha5"]
                  [duct/compiler.cljs "0.2.0"]
                  [duct/server.figwheel "0.2.1"]
-                 [integrant "0.7.0-alpha1"]])
+                 [integrant "0.7.0-alpha2"]])
