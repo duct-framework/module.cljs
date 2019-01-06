7a377a38199f3470f304d62d61ab8e88b8d1249e
James Reeves <jreeves@weavejester.com>
2019-01-06 13:29:38 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/duct-framework/module.cljs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.339"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/clojurescript "1.10.439"]
                  [binaryage/devtools "0.9.10"]
-                 [duct/core "0.7.0-beta1"]
-                 [duct/compiler.cljs "0.3.0-beta1"]
-                 [duct/server.figwheel "0.3.0-beta1"]
+                 [duct/core "0.7.0"]
+                 [duct/compiler.cljs "0.3.0"]
+                 [duct/server.figwheel "0.3.0"]
                  [integrant "0.7.0"]])
