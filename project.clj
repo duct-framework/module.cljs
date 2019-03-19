cc0cbf0e5a82c144304dfa3c1ff6e60afac3acb4
James Reeves <jreeves@weavejester.com>
2019-03-19 15:27:03 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.439"]
+                 [org.clojure/clojurescript "1.10.520"]
                  [binaryage/devtools "0.9.10"]
                  [duct/core "0.7.0"]
                  [duct/compiler.cljs "0.3.0"]
-                 [duct/server.figwheel "0.3.0"]
+                 [duct/server.figwheel "0.3.1"]
                  [integrant "0.7.0"]])
